bddc0032a8095bdd7869c33ccf36baff7aed773f
nagaraj <nagarajthangaraj356@gmail.com>
2023-09-25 13:31:58 +0530

    still developing...

---
+++ src/sourceData/sourceData.js
@@ -0,0 +1,60 @@
+const myInformation = {
+  myMessage: "Hii!",
+  myName: "Nagaraj Thangaraj",
+  jobRole: "Front End Developer",
+  gitHub: "https://github.com/Nagaraj-thangaraj",
+  linkedIn: "https://www.linkedin.com/in/nagaraj-thangaraj-2ba836247/",
+  gMail: "nagarajthangaraj872@gmail.com",
+  aboutMySelf1: "Hi Everyone, I am Nagaraj Thangaraj from Salem, India",
+  aboutMySelf2: "I am an innovative and dedicated hardworking front end developer with hands-on experience in developing web applications with React.",
+  aboutMySelf3:"I spend most days learning and building up, either start it from scratch and adding some customized features and functionality.",
+  Languages: [
+    {
+      Language: "HTML 5",
+      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    },
+    {
+      Language: "CSS 3",
+      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    },
+    {
+      Language: "JavaScript",
+      img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    },
+  ],
+  frameworksAndLibrary: [
+    {
+      Language: "React JS",
+      Img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    },
+
+    {
+      Language: "Bootstrap",
+      Img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+    },
+
+    {
+      Language: "Material UI",
+      Img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+    },
+    {
+      Language: "Moment.js",
+      Img: "https://cdn.worldvectorlogo.com/logos/momentjs.svg",
+    },
+  ],
+  tools: [
+    {
+      tool: "VS Code",
+      Img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+    },
+    {
+      tool: "Git",
+      Img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    },
+    {
+      tool: "GitHub",
+      Img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg",
+    },
+    { tool: "Vercel", img: "https://sivanathan.info/asset/vercel.svg" },
+  ],
+};
